9a144be036196399db166e0202ded44f0f174c8b
Harrison W. Rodrigues <lucas.hurmann@hotmail.com>
2024-09-11 16:33:27 -0300

    First Steps

---
+++ script.js
@@ -0,0 +1,33 @@
+const form = document.querySelector('form')
+const amount = document.querySelector('#amount')
+const expense = document.querySelector('#expense')
+const category = document.querySelector('#category')
+
+amount.oninput = () => {
+  let value = amount.value.replace(/\D/g, "")
+
+  value = Number(value) / 100
+
+  amount.value = formatCurrencyBRL(value)
+}
+
+function formatCurrencyBRL(value) {
+  value = value.toLocaleString('pt-BR', {
+    style: 'currency',
+    currency: 'BRL',
+  })
+  return value
+}
+
+form.onsubmit = (event) => {
+  event.preventDefault()
+
+  const newExpense = {
+    id: new Date().getTime(),
+    expense: expense.value,
+    category_id: category.value,
+    category_name: category.options[category.selectedIndex].text,
+    amount: amount.value,
+    created_at: new Date(),
+  }
+}
